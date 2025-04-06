1f4ce9686724da470f305d8aaae3d79579d2219a
giathinhnguyen <nguyengiathinh231106@gmail.com>
2025-04-06 22:34:59 +0700

    new project

---
+++ ThapHaNoi.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void thap(int n,char nguon,char giua,char dich){
+    if(n==1){
+        cout<<nguon<<" -->"<<dich<<endl;
+    } else {
+        thap(n-1,nguon,dich,giua);// n-1 dia tren ve cot giua
+        thap(1,nguon,giua,dich);  // dia cuoi ve dich
+        thap(n-1,giua,nguon,dich);// n-1 dia tren tu cot giua ve dich
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    thap(n,'A','B','C');
+    return 0;
+}
